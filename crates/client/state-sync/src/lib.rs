2caa7f0ac76aff23cb40314d12a8c486f2dcf1cd
jerrybaoo <shirui_yj133@163.com>
2023-11-16 16:56:26 +0800

    apply state diff

--- crates/client/state-sync/src/lib.rs
+++ crates/client/state-sync/src/lib.rs
@@ -3,13 +3,22 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use async_trait::async_trait;
+use blockifier::state::cached_state::CommitmentStateDiff;
 use ethers::types::I256;
 use mp_transactions::Transaction;
-use sc_client_api::backend::Backend;
+use sc_client_api::backend::{Backend, BlockImportOperation};
 use sp_blockchain::HeaderBackend;
 use sp_runtime::traits::Block as BlockT;
 use starknet_api::block::{BlockHash, BlockNumber};
 use starknet_api::state::StateDiff;
+use starknet_api::api_core::{ClassHash, CompiledClassHash, ContractAddress, Nonce, PatriciaKey};
+use starknet_api::hash::StarkFelt;
+use starknet_api::state::StorageKey as StarknetStorageKey;
+use mp_storage::{SN_COMPILED_CLASS_HASH_PREFIX, SN_CONTRACT_CLASS_HASH_PREFIX, SN_NONCE_PREFIX, SN_STORAGE_PREFIX};
+use indexmap::IndexMap;
+use frame_support::{Identity, StorageHasher};
+use sp_state_machine::{ StorageKey, StorageValue};
+use sp_core::Encode;
 
 #[cfg(test)]
 pub mod tests;
@@ -26,8 +35,6 @@ pub trait L1StateProvider {
 pub struct StateSyncWorker<B: BlockT, C, BE> {
     client: Arc<C>,
     substrate_backend: Arc<BE>,
-    madara_backend: Arc<mc_db::Backend<B>>,
-    l1_state_provider: Box<dyn L1StateProvider>,
     phantom_data: PhantomData<B>,
 }
 
@@ -39,10 +46,8 @@ where
     pub fn new(
         client: Arc<C>,
         substrate_backend: Arc<BE>,
-        madara_backend: Arc<mc_db::Backend<B>>,
-        l1_state_provider: Box<dyn L1StateProvider>,
     ) -> Self {
-        Self { client, substrate_backend, l1_state_provider, madara_backend, phantom_data: PhantomData }
+        Self { client, substrate_backend, phantom_data: PhantomData }
     }
 
     // Apply the state difference to the data layer.
@@ -50,7 +55,143 @@ where
     // and the madara blockchain does not have an independent state root,
     // we temporarily use the highest Substrate block as the latest state.
     // Then, we apply the state difference to the state represented by the state root of this block.
-    fn apply_state_diff(&mut self, state_diff: StateDiff) {
-        // Backend::begin_state_operation, Backend::commit_operation. 
+    fn apply_state_diff(&mut self, commitment_state_diff: CommitmentStateDiff) {
+        // Backend::begin_state_operation, Backend::commit_operation.
+        let block_info = self.client.info();
+        //let block_import_operation = BlockImportOperation::<B>::default();
+        // self.substrate_backend.begin_state_operation();
+        let mut operation = self.substrate_backend.begin_operation().unwrap();
+        let storage_changes: InnerStorageChangeSet = commitment_state_diff.into();
+        operation.update_storage(storage_changes.changes, storage_changes.child_changes).unwrap();
+        self.substrate_backend.begin_state_operation(&mut operation, block_info.best_hash).unwrap();
+        self.substrate_backend.commit_operation(operation).unwrap();
     }
+}
+
+// InnerStorageChangeSet just used for test
+#[derive(Debug, PartialEq, Eq)]
+struct InnerStorageChangeSet {
+    changes: Vec<(StorageKey, Option<StorageValue>)>,
+    child_changes: Vec<(StorageKey, Vec<(StorageKey, Option<StorageValue>)>)>,
+}
+
+impl InnerStorageChangeSet {
+    pub fn iter(&self) -> impl Iterator<Item = (Option<&StorageKey>, &StorageKey, Option<&StorageValue>)> + '_ {
+        let top = self.changes.iter().map(|(k, v)| (None, k, v.as_ref()));
+        let children = self
+            .child_changes
+            .iter()
+            .flat_map(|(sk, changes)| changes.iter().map(move |(k, v)| (Some(sk), k, v.as_ref())));
+        top.chain(children)
+    }
+}
+
+impl Into<CommitmentStateDiff> for InnerStorageChangeSet {
+    fn into(self) -> CommitmentStateDiff {
+        let mut commitment_state_diff = CommitmentStateDiff {
+            address_to_class_hash: Default::default(),
+            address_to_nonce: Default::default(),
+            storage_updates: Default::default(),
+            class_hash_to_compiled_class_hash: Default::default(),
+        };
+
+        for (_prefix, full_storage_key, change) in self.iter() {
+            // The storages we are interested in all have prefix of length 32 bytes.
+            // The pallet identifier takes 16 bytes, the storage one 16 bytes.
+            // So if a storage key is smaller than 32 bytes,
+            // the program will panic when we index it to get it's prefix
+            if full_storage_key.len() < 32 {
+                continue;
+            }
+            let prefix = &full_storage_key[..32];
+
+            // All the `try_into` are safe to `unwrap` because we know what the storage contains
+            // and therefore what size it is
+            if prefix == *SN_NONCE_PREFIX {
+                let contract_address =
+                    ContractAddress(PatriciaKey(StarkFelt(full_storage_key[32..].try_into().unwrap())));
+                // `change` is safe to unwrap as `Nonces` storage is `ValueQuery`
+                let nonce = Nonce(StarkFelt(change.unwrap().clone().try_into().unwrap()));
+                commitment_state_diff.address_to_nonce.insert(contract_address, nonce);
+            } else if prefix == *SN_STORAGE_PREFIX {
+                let contract_address =
+                    ContractAddress(PatriciaKey(StarkFelt(full_storage_key[32..64].try_into().unwrap())));
+                let storage_key =
+                    StarknetStorageKey(PatriciaKey(StarkFelt(full_storage_key[64..].try_into().unwrap())));
+                // `change` is safe to unwrap as `StorageView` storage is `ValueQuery`
+                let value = StarkFelt(change.unwrap().clone().try_into().unwrap());
+
+                match commitment_state_diff.storage_updates.get_mut(&contract_address) {
+                    Some(contract_storage) => {
+                        contract_storage.insert(storage_key, value);
+                    }
+                    None => {
+                        let mut contract_storage: IndexMap<_, _, _> = Default::default();
+                        contract_storage.insert(storage_key, value);
+
+                        commitment_state_diff.storage_updates.insert(contract_address, contract_storage);
+                    }
+                }
+            } else if prefix == *SN_CONTRACT_CLASS_HASH_PREFIX {
+                let contract_address =
+                    ContractAddress(PatriciaKey(StarkFelt(full_storage_key[32..].try_into().unwrap())));
+                // `change` is safe to unwrap as `ContractClassHashes` storage is `ValueQuery`
+                let class_hash = ClassHash(StarkFelt(change.unwrap().clone().try_into().unwrap()));
+
+                commitment_state_diff.address_to_class_hash.insert(contract_address, class_hash);
+            } else if prefix == *SN_COMPILED_CLASS_HASH_PREFIX {
+                let class_hash = ClassHash(StarkFelt(full_storage_key[32..].try_into().unwrap()));
+                // In the current state of starknet protocol, a compiled class hash can not be erased, so we should
+                // never see `change` being `None`. But there have been an "erase contract class" mechanism live on
+                // the network during the Regenesis migration. Better safe than sorry.
+                let compiled_class_hash = CompiledClassHash(
+                    change.map(|data| StarkFelt(data.clone().try_into().unwrap())).unwrap_or_default(),
+                );
+
+                commitment_state_diff.class_hash_to_compiled_class_hash.insert(class_hash, compiled_class_hash);
+            }
+        }
+
+        commitment_state_diff
+    }
+}
+
+impl From<CommitmentStateDiff> for InnerStorageChangeSet {
+    fn from(commitment_state_diff: CommitmentStateDiff) -> Self {
+        let mut changes: Vec<(StorageKey, Option<StorageValue>)> = Vec::new();
+        // now starknet not use child changes.
+        let mut _child_changes: Vec<(StorageKey, Vec<(StorageKey, Option<StorageValue>)>)> = Vec::new();
+
+        for (address, class_hash) in commitment_state_diff.address_to_class_hash.iter() {
+            let storage_key = storage_key_build(SN_CONTRACT_CLASS_HASH_PREFIX.clone(), &address.encode());
+            let storage_value = class_hash.encode();
+            changes.push((storage_key, Some(storage_value)));
+        }
+
+        for (address, nonce) in commitment_state_diff.address_to_nonce.iter() {
+            let storage_key = storage_key_build(SN_NONCE_PREFIX.clone(), &address.encode());
+            let storage_value = nonce.encode();
+            changes.push((storage_key, Some(storage_value)));
+        }
+
+        for (address, storages) in commitment_state_diff.storage_updates.iter() {
+            for (sk, value) in storages.iter(){
+                let storage_key = storage_key_build(SN_STORAGE_PREFIX.clone(),&[address.encode(), sk.encode()].concat());
+                let storage_value = value.encode();
+                changes.push((storage_key, Some(storage_value)));
+            }
+        }
+
+        for (address, compiled_class_hash) in commitment_state_diff.class_hash_to_compiled_class_hash.iter() {
+            let storage_key = storage_key_build(SN_COMPILED_CLASS_HASH_PREFIX.clone(), &address.encode());
+            let storage_value = compiled_class_hash.encode();
+            changes.push((storage_key, Some(storage_value)));
+        }
+
+        InnerStorageChangeSet { changes, child_changes: _child_changes }
+    }
+}
+
+pub fn storage_key_build(prefix: Vec<u8>, key: &[u8]) -> Vec<u8> {
+    [prefix, Identity::hash(key)].concat()
 }
--- crates/client/state-sync/src/tests/mod.rs
+++ crates/client/state-sync/src/tests/mod.rs
@@ -1,12 +1,32 @@
 //! Madara client testing utilities.
+pub mod constants;
+pub mod helpers;
 
-pub use substrate_test_client::*;
-pub use madara_runtime::{WASM_BINARY, GenesisConfig, BuildStorage, SystemConfig};
+use std::sync::Arc;
+use blockifier::state::cached_state::CommitmentStateDiff;
+use constants::*;
+use frame_support::assert_ok;
+use futures::executor::block_on;
+use helpers::*;
 pub use madara_runtime as runtime;
+pub use madara_runtime::{
+    BuildStorage, GenesisConfig, RuntimeCall, SealingMode, SystemConfig, UncheckedExtrinsic, WASM_BINARY,
+};
+use mp_felt::Felt252Wrapper;
+use mp_transactions::InvokeTransactionV1;
 use pallet_starknet::genesis_loader::{GenesisData, GenesisLoader};
+use pallet_starknet::Call as StarknetCall;
+use sc_block_builder::{BlockBuilderProvider, RecordProof};
+use sc_client_api::ExecutionStrategy::NativeElseWasm;
+use sc_client_api::HeaderBackend;
+use sp_consensus::BlockOrigin;
+use sp_inherents::InherentData;
+use sp_state_machine::BasicExternalities;
+use sp_timestamp::{Timestamp, INHERENT_IDENTIFIER};
+pub use substrate_test_client::*;
+use super::*;
 
 pub type Backend = sc_client_db::Backend<runtime::Block>;
-
 pub struct MadaraExecutorDispatch;
 impl sc_executor::NativeExecutionDispatch for MadaraExecutorDispatch {
     /// Only enable the benchmarking host functions when we actually want to benchmark.
@@ -29,71 +49,152 @@ pub type ExecutorDispatch = sc_executor::NativeElseWasmExecutor<MadaraExecutorDi
 
 /// Test client type.
 pub type Client = client::Client<
-	Backend,
-	client::LocalCallExecutor<runtime::Block, Backend, ExecutorDispatch>,
-	runtime::Block,
-	runtime::RuntimeApi,
+    Backend,
+    client::LocalCallExecutor<runtime::Block, Backend, ExecutorDispatch>,
+    runtime::Block,
+    runtime::RuntimeApi,
 >;
 
 #[derive(Default)]
 pub struct GenesisParameters;
 
 impl substrate_test_client::GenesisInit for GenesisParameters {
-	fn genesis_storage(&self) -> Storage {
+    fn genesis_storage(&self) -> Storage {
         let genesis_data: GenesisData = serde_json::from_str(std::include_str!("./genesis.json")).unwrap();
         let genesis_loader = GenesisLoader::new(project_root::get_project_root().unwrap(), genesis_data);
-        
+
         let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string()).unwrap();
-        GenesisConfig {
-            system: SystemConfig {
-                code: wasm_binary.to_vec(),
-            },
+
+        let mut storage = GenesisConfig {
+            system: SystemConfig { code: wasm_binary.to_vec() },
             aura: Default::default(),
             grandpa: Default::default(),
             starknet: genesis_loader.into(),
-        }.build_storage().unwrap()
-	}
+        }
+        .build_storage()
+        .unwrap();
+
+        BasicExternalities::execute_with_storage(&mut storage, || {
+            madara_runtime::Sealing::set(&SealingMode::Manual);
+        });
+
+        storage
+    }
 }
 
-pub type TestClientBuilder<E, B> = substrate_test_client::TestClientBuilder<
-	runtime::Block,
-	E,
-	B,
-	GenesisParameters,
->;
+pub type TestClientBuilder<E, B> = substrate_test_client::TestClientBuilder<runtime::Block, E, B, GenesisParameters>;
 
 pub trait TestClientBuilderExt: Sized {
-	/// Create test client builder.
-	fn new() -> Self;
+    /// Create test client builder.
+    fn new() -> Self;
 
-	/// Build the test client.
-	fn build(self) -> Client;
+    /// Build the test client.
+    fn build(self) -> (Client, Arc<Backend>);
 }
 
 impl TestClientBuilderExt
-	for substrate_test_client::TestClientBuilder<
-		runtime::Block,
-		client::LocalCallExecutor<runtime::Block, Backend, ExecutorDispatch>,
-		Backend,
-		GenesisParameters,
-	>
+    for substrate_test_client::TestClientBuilder<
+        runtime::Block,
+        client::LocalCallExecutor<runtime::Block, Backend, ExecutorDispatch>,
+        Backend,
+        GenesisParameters,
+    >
 {
-	fn new() -> Self {
-		Self::default()
-	}
+    fn new() -> Self {
+        Self::default()
+    }
 
-	fn build(self) -> Client {
-		self.build_with_native_executor(None).0
-	}
+    fn build(self) -> (Client, Arc<Backend>) {
+        let backend = self.backend();
+        (self.set_execution_strategy(NativeElseWasm).build_with_native_executor(None).0, backend)
+    }
+}
+
+// create_test_client with has apply genesis then build and import the first block.
+pub fn create_test_client() -> (Client, Arc<Backend>) {
+    let (mut client, backend) = TestClientBuilder::new().build();
+
+    let block_info = client.info();
+    let mut builder = client.new_block_at(block_info.best_hash, Default::default(), RecordProof::Yes).unwrap();
+    let mut inherent_data = InherentData::new();
+    inherent_data.put_data(INHERENT_IDENTIFIER, &Timestamp::new(100)).unwrap();
+
+    let inherent_exts = builder.create_inherents(inherent_data).unwrap();
+    for ex in inherent_exts {
+        builder.push(ex).unwrap();
+    }
+
+    let block = builder.build().unwrap();
+
+    assert_ok!(block_on(client.import(BlockOrigin::Own, block.block)));
+    (client, backend)
 }
 
 #[test]
-fn test_basic_state_diff(){
-	let mut _client = TestClientBuilder::new().build();
-	// 1. make block (transfer, deploy)
-	// 2. client new block builder
-	// 3. block builder build block, get {block, state changes, applied state root}
-	// 4. get state diff from state changes
-	// 4. apply state diff to client. 
-	// 5. check starknet contract state by runtime api
+fn test_basic_state_diff() {
+    // 1. make block (transfer, deploy)
+    // 2. client new block builder
+    // 3. block builder build block, get {block, state changes, applied state root}
+    // 4. get state diff from state changes
+    // 4. apply state diff to backend.
+    // 5. check starknet contract state by runtime api
+    let (client, backend) = create_test_client();
+
+    let sender_account = get_account_address(None, AccountType::V0(AccountTypeV0Inner::NoValidate));
+    let felt_252_sender_account = sender_account.into();
+    // ERC20 is already declared for the fees.
+    // Deploy ERC20 contract
+    let deploy_transaction = InvokeTransactionV1 {
+        max_fee: u128::MAX,
+        signature: vec![],
+        nonce: Felt252Wrapper::ZERO,
+        sender_address: felt_252_sender_account,
+        calldata: vec![
+            felt_252_sender_account, // Simple contract address
+            Felt252Wrapper::from_hex_be("0x02730079d734ee55315f4f141eaed376bddd8c2133523d223a344c5604e0f7f8").unwrap(), /* deploy_contract selector */
+            Felt252Wrapper::from_hex_be("0x9").unwrap(), // Calldata len
+            Felt252Wrapper::from_hex_be(TOKEN_CONTRACT_CLASS_HASH).unwrap(), // Class hash
+            Felt252Wrapper::ONE,                         // Contract address salt
+            Felt252Wrapper::from_hex_be("0x6").unwrap(), // Constructor_calldata_len
+            Felt252Wrapper::from_hex_be("0xA").unwrap(), // Name
+            Felt252Wrapper::from_hex_be("0x1").unwrap(), // Symbol
+            Felt252Wrapper::from_hex_be("0x2").unwrap(), // Decimals
+            Felt252Wrapper::from_hex_be("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").unwrap(), // Initial supply low
+            Felt252Wrapper::from_hex_be("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").unwrap(), // Initial supply high
+            felt_252_sender_account,                     // recipient
+        ],
+    };
+
+    let call: RuntimeCall =
+        StarknetCall::invoke { transaction: mp_transactions::InvokeTransaction::V1(deploy_transaction) }.into();
+    let ext = UncheckedExtrinsic { signature: None, function: call };
+
+    let block_info = client.info();
+
+    let mut builder = client.new_block_at(block_info.best_hash, Default::default(), RecordProof::Yes).unwrap();
+    let mut inherents = InherentData::new();
+    inherents
+        .put_data(INHERENT_IDENTIFIER, &Timestamp::new(6000u64 * (block_info.best_number as u64 + 1) + 2))
+        .unwrap();
+
+    let inherents_exs = builder.create_inherents(inherents).unwrap();
+    for ex in inherents_exs {
+        builder.push(ex).unwrap();
+    }
+    builder.push(ext).unwrap();
+
+    let block = builder.build().unwrap();
+    let ics = InnerStorageChangeSet {
+        changes: block.storage_changes.main_storage_changes.clone(),
+        child_changes: block.storage_changes.child_storage_changes.clone(),
+    };
+
+    let commitment_state_diff: CommitmentStateDiff = ics.into();
+    let ics2 = InnerStorageChangeSet::from(commitment_state_diff.clone());
+    let commitment_state_diff2: CommitmentStateDiff = ics2.into();
+
+    assert_eq!(commitment_state_diff, commitment_state_diff2);
+
+    let mut sync_worker = StateSyncWorker::new(Arc::new(client), backend);
+    sync_worker.apply_state_diff(commitment_state_diff2);
 }

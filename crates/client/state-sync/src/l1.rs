c606a70b76153e010eef261737cc18654651fb15
jerrybaoo <61228234+jerrybaoo@users.noreply.github.com>
2023-12-05 11:30:53 +0800

    init state-sync crate

---
+++ crates/client/state-sync/src/l1.rs
@@ -0,0 +1,44 @@
+use std::{time::Duration, num::NonZeroU64};
+use primitive_types::H160;
+
+// use tokio::sync::mpsc;
+
+use crate::ethereum::EthereumApi;
+
+use super::retry::Retry;
+
+
+#[derive(Clone)]
+pub struct L1SyncContext<EthereumClient> {
+    pub ethereum: EthereumClient,
+    /// The Starknet core contract address on Ethereum
+    pub core_address: H160,
+    pub poll_interval: Duration,
+}
+
+
+pub async fn sync_from_l1_loop<T>(
+    context: L1SyncContext<T>,
+) -> anyhow::Result<()>
+where
+    T: EthereumApi<H> + Clone,
+{
+    let L1SyncContext {
+        ethereum,
+        core_address,
+        poll_interval,
+    } = context;
+
+	loop {
+        let state_update = Retry::exponential(
+            || async { ethereum.get_starknet_state(&core_address).await },
+            NonZeroU64::new(1).unwrap(),
+        )
+        .factor(NonZeroU64::new(2).unwrap())
+        .max_delay(poll_interval / 2)
+        .when(|_| true)
+        .await?;
+
+        tokio::time::sleep(poll_interval).await;
+    }
+}
